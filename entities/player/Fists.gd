9b01716aa5d8356491e0efa0893a75d08f55ed97
Artem <60270850+artemvandalovski@users.noreply.github.com>
2024-02-21 18:43:48 -0500

    player punch animation

--- entities/player/Fists.gd
+++ entities/player/Fists.gd
@@ -2,31 +2,47 @@ extends Node2D
 
 const PUNCH_DEPTH = 16.0 # how far the fists will go for a punch
 const CHARGE_DEPTH = 8.0 # how far the fists will go back while charging
-const COOLDOWN = 0.04
+const COOLDOWN = 0.07
+const CHARGE_RATE = 2.0
+const MAX_CHARGE = -1
 const MIN_KNOCKBACK = 200.0
 
-@export var charge_rate: float = 2.0
-@export var punch_force: float = 300.0 # how much the player will launch forward when punching
-@export var knockback: float = 400.0
-@export var dmg: int = 0 # base damage
-@export var full_dmg: int = 2 # damage when fully charged
+const L_CLICK = "left_click"
+const R_CLICK = "right_click"
 
-@onready var l_fist = $LFist
-@onready var r_fist = $RFist
+@onready var l_fist = $LeftFist
+@onready var r_fist = $RightFist
 
-var lcharge: float = 0.0 # between -1 and 1
-var rcharge: float = 0.0 # between -1 and 1
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+var l_charge = 0.0
+var r_charge = 0.0
 
 
 func _process(delta):
-	if Input.is_action_pressed('lclick') and lcharge <= 0.0:
-		lcharge = max(lcharge - charge_rate*delta, -1.0)
-		$LFist.position.x = lcharge * CHARGE_DEPTH
+	handle_charge(delta)
+	handle_punch(delta)
+
+
+func handle_charge(delta):
+	if Input.is_action_pressed(L_CLICK):
+		l_charge = charge(l_charge, delta)
+		l_fist.position.x = l_charge * CHARGE_DEPTH
 	
-	if Input.is_action_pressed('rclick') and rcharge <= 0.0:
-		rcharge = max(rcharge - charge_rate*delta, -1.0)
-		$RFist.position.x = rcharge * CHARGE_DEPTH
+	if Input.is_action_pressed(R_CLICK):
+		r_charge = charge(r_charge, delta)
+		r_fist.position.x = r_charge * CHARGE_DEPTH
+
+func handle_punch(delta):
+	if Input.is_action_just_released(L_CLICK):
+		punch(l_fist, l_charge)
+		l_charge = 0.0
+	if Input.is_action_just_released(R_CLICK):
+		punch(r_fist, r_charge)
+		r_charge = 0.0
+
+func charge(charge: float, delta):
+	return max(charge - CHARGE_RATE * delta, MAX_CHARGE)
+
+func punch(fist: Node2D, charge: float):
+	var punch_tween = create_tween().set_trans(Tween.TRANS_QUINT).set_ease(Tween.EASE_OUT)
+	punch_tween.tween_property(fist, 'position:x', charge + PUNCH_DEPTH, 0.1)
+	punch_tween.tween_property(fist, 'position:x', 0, COOLDOWN)
