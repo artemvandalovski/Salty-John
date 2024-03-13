24a9b7a15844372c957500bf5aaef0b968d10d20
Artem <60270850+artemvandalovski@users.noreply.github.com>
2024-03-13 01:57:50 -0400

    working dragging feature

--- environment/draggable/draggable.gd
+++ environment/draggable/draggable.gd
@@ -2,7 +2,10 @@ class_name Draggable
 extends RigidBody2D
 
 @export var spin_force = 0
-@export var velocity_treshhold = 125.0
+@export var velocity_treshhold = 400.0
+
+#func _ready():
+	#$Hitbox.knockback_func = get_knockback
 
 func take_knockback(kb: Vector2):
 	apply_torque_impulse(kb.length() * randi_range(-2, 2) * spin_force)
@@ -17,3 +20,11 @@ func get_knockback():
 
 func is_fast_enough() -> bool:
 	return linear_velocity.length_squared() > pow(velocity_treshhold, 2)
+
+
+func _on_hitbox_area_entered(hurtbox: Hurtbox):
+	assert(hurtbox is Hurtbox)
+	
+	if is_fast_enough():
+		hurtbox.apply_knockback(get_knockback())
+		hurtbox.apply_damage(1)
--- player/fists/fist.gd
+++ player/fists/fist.gd
@@ -1,54 +1,55 @@
 extends Weapon
 
 const PUNCH_DEPTH = 16 # how far the fists will go for a punch
-const CHARGE_DEPTH = 8 # how far the fists will go back while charging
+const CHARGE_DEPTH = 16 # how far the fists will go back while charging
 const CHARGE_RATE = 2
-const MAX_CHARGE = -1
+const MAX_CHARGE = 2
 const COOLDOWN = 0.07
+
+const KNOCKBACK_STRENGTH: float = 200.0
+const KNOCKBACK_MIN: float = 100.0
 
 @export_enum("left_click", "right_click") var input: String
 
 @onready var hitbox = $Hitbox
-@onready var knockback_component = $Knockback
-@onready var dragging_raycast = $Dragging
 
 var charge = 0.0
 
+func _ready():
+	hitbox.knockback_func = calc_knockback
 
-func _process(delta):
+# Gets called from parent (See Fists)
+# The reason for this is to give us more control over when and how these
+#  children fists get updated
+# E.g. We don't want them to handle attacking while we're dragging a chair
+func update(delta: float):
 	handle_charge(delta)
-	handle_punch(delta)
+	handle_punch()
 
 
 func handle_charge(delta):
 	if Input.is_action_pressed(input):
-		handle_dragging()
-		charge = max(charge - CHARGE_RATE * delta, MAX_CHARGE)
-		position.x = charge * CHARGE_DEPTH
+		charge = minf(charge + CHARGE_RATE * delta, MAX_CHARGE)
+		position.x = -CHARGE_DEPTH * (charge / MAX_CHARGE)
 
-func handle_punch(delta):
+func handle_punch():
 	if Input.is_action_just_released(input):
 		punch()
 
 func punch():
-	hitbox.collision.disabled = false
-	var punch_tween = create_tween().set_trans(Tween.TRANS_QUINT).set_ease(Tween.EASE_OUT)
+	# Animateji
+	var punch_tween: Tween = create_tween()\
+		.set_trans(Tween.TRANS_QUINT).set_ease(Tween.EASE_OUT)
 	punch_tween.tween_property(self, 'position:x', charge + PUNCH_DEPTH, 0.1)
+	punch_tween.tween_callback(hitbox.deal_damage) # <-- DAMAGE IS DEALT HERE
 	punch_tween.tween_property(self, 'position:x', 0, COOLDOWN)
+	
 	await punch_tween.finished
-	hitbox.collision.disabled = true
+	
 	charge = 0.0
 
-func get_knockback() -> Vector2:
-	var power = charge * -1
-	var strength = knockback_component.strength
-	var min_knockback = knockback_component.min_knockback
+func calc_knockback(_hurtbox: Hurtbox) -> Vector2:
 	var velocity = owner.holder.velocity
-	var direction = Vector2(cos(global_rotation), sin(global_rotation))
-	var knockback = velocity + (direction * max(strength * power, min_knockback))
+	var direction = Vector2.from_angle(global_rotation)
+	var knockback = velocity + (direction * max(KNOCKBACK_STRENGTH * charge, KNOCKBACK_MIN))
 	return knockback
-
-func handle_dragging():
-	if !dragging_raycast.is_colliding(): return
-	var draggable_object: RigidBody2D = dragging_raycast.get_collider().owner
-	draggable_object.global_position = lerp(draggable_object.global_position, global_position, 0.05)
