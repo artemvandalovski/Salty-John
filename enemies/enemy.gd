c3b930af1233a242e55b3ebf08a6f7ad35bb8331
Artem <60270850+artemvandalovski@users.noreply.github.com>
2024-03-27 00:44:30 -0400

    3 hours of trying to context steering :(

--- enemies/enemy.gd
+++ enemies/enemy.gd
@@ -3,10 +3,15 @@ extends CharacterBody2D
 
 const FRICTION = 0.85
 
+const speed = 200
+
 @export var health := 3
 @export var mass := 1
 
 @onready var label = $Label
+@onready var nav_agent = $NavigationAgent2D
+
+@onready var player: Node2D = Global.player
 
 func _ready():
 	update_label()
@@ -18,6 +23,9 @@ func update_label():
 	label.text = str(health) + "HP"
 
 func handle_movement():
+	#make_path()
+	#var dir = to_local(nav_agent.get_next_path_position()).normalized()
+	#velocity = dir * speed
 	velocity *= FRICTION
 	move_and_slide()
 
@@ -29,3 +37,6 @@ func take_damage(dmg: int):
 
 func take_knockback(kb: Vector2):
 	velocity += kb / mass
+
+func make_path():
+	nav_agent.target_position = player.global_position
