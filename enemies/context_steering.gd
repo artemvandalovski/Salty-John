232ba686440e5077faeb91950d5288fe1fcaf8c9
Artem <60270850+artemvandalovski@users.noreply.github.com>
2024-04-10 00:50:15 -0400

    working context steering

--- enemies/context_steering.gd
+++ enemies/context_steering.gd
@@ -11,31 +11,31 @@ var angle_step = TAU / NUM_RAYS
 func _ready():
 	generate_raycasts()
 
-func _process(delta):
-	calc_interest()
-
-func _draw():
-	for i in range(NUM_RAYS):
-		var ray: RayCast2D = get_child(i)
-		var direction: Vector2 = ray.target_position
-		
-		var forward_direction = to_local(player.global_position)
-		var dot_product = direction.dot(forward_direction)
-		var importance = clamp(dot_product, 0.0, 1.0)
-		print(importance)
-		var interest = direction * importance
-		
-		draw_line(Vector2(), interest, Color.GREEN, 1.0)
-
 
 func generate_raycasts():
 	for i in range(NUM_RAYS):
 		var ray = RayCast2D.new()
 		var direction = Vector2(cos(i * angle_step), sin(i * angle_step)) * RAY_LENGTH
-		ray.collision_mask = 2
-		ray.add_exception(owner)
+		ray.collision_mask = 5
 		ray.set_target_position(direction)
 		add_child(ray)
 
-func calc_interest():
-	var forward_direction = to_local(player.global_position)
+func calc_steering() -> Vector2:
+	var result = Vector2.ZERO
+	var target = to_local(player.global_position)
+	for i in range(NUM_RAYS):
+		var ray: RayCast2D = get_child(i)
+		var direction = ray.target_position.normalized()
+		var interest = calc_interest(direction, target)
+		var danger = calc_danger(ray) * 5
+		result += direction * (interest - danger)
+	return result.normalized()
+
+func calc_interest(direction: Vector2, target: Vector2) -> float:
+	var dot_product = direction.dot(target)
+	return clamp(dot_product, 0.0, 1.0)
+	
+func calc_danger(ray: RayCast2D) -> float:
+	if !ray.is_colliding(): return 0.0
+	var collision_distance = ray.get_collision_point().distance_to(global_position)
+	return clamp(1.0 - collision_distance / RAY_LENGTH, 0.0, 1.0)
--- enemies/enemy.gd
+++ enemies/enemy.gd
@@ -2,16 +2,17 @@ class_name Enemy
 extends CharacterBody2D
 
 const FRICTION = 0.85
-
-const speed = 200
+const SPEED = 40
 
 @export var health := 3
 @export var mass := 1
 
 @onready var label = $Label
 @onready var nav_agent = $NavigationAgent2D
+@onready var context_steering = $ContextSteering
 
 @onready var player: Node2D = Global.player
+
 
 func _ready():
 	update_label()
@@ -19,10 +20,9 @@ func _ready():
 func _physics_process(delta):
 	handle_movement()
 
-func update_label():
-	label.text = str(health) + "HP"
 
 func handle_movement():
+	velocity += direction * SPEED
 	velocity *= FRICTION
 	move_and_slide()
 
@@ -34,3 +34,6 @@ func take_damage(dmg: int):
 
 func take_knockback(kb: Vector2):
 	velocity += kb / mass
+
+func update_label():
+	label.text = str(health) + "HP"
