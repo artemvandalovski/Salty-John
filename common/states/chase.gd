c1d6652fc871ba6a5a2a27724c03943336ba6658
Artem <60270850+artemvandalovski@users.noreply.github.com>
2024-05-06 09:00:55 -0400

    refactor context steering

--- common/states/chase.gd
+++ common/states/chase.gd
@@ -5,5 +5,8 @@ var movement_func: Callable
 
 @onready var context_steerer = $"../../ContextSteerer"
 
+func enter():
+	owner.target = Global.player
+
 func process(delta):
 	owner.direction = context_steerer.calc_steering()
